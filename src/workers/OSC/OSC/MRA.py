fcb35c9a75207e5b6dbabb425330671d0487bfdf
liehr <liehr@3321fe91-ea1f-0410-9b6b-81c2d13ccd49>
2008-09-05 10:46:24 +0000

    Small bug-fixes and modifications of chi-by-eye testing.
    git-svn-id: https://pyphant.svn.sourceforge.net/svnroot/pyphant/trunk@530 3321fe91-ea1f-0410-9b6b-81c2d13ccd49

--- src/workers/OSC/OSC/MRA.py
+++ src/workers/OSC/OSC/MRA.py
@@ -68,7 +68,7 @@ class MRA(Worker.Worker):
         minima_c = numpy.logical_and(fieldData[:-2] > fieldData[1:-1],
                                    fieldData[2:]  > fieldData[1:-1])
         minima = minima_c.nonzero()[0]
-        if lastExtrema==None:
+        if lastExtrema==None or len(lastExtrema)==len(minima):
             return minima
         trackedMinima = []
         for lastMinimum in lastExtrema:
@@ -87,15 +87,17 @@ class MRA(Worker.Worker):
         assert numpy.allclose(d.min(), d.max())
         n = scale/d[0]*dim.unit
         mrr = [self.convolve(field, sigma) for sigma in
-               numpy.linspace(1, n,100).tolist()]
+               numpy.linspace(1, n,10).tolist()]
+        print len(field.data),len(mrr[0])
+        mrr.insert(0,field.data)
         lastMinima = None
         origExtremaPos = None
         for row in mrr.__reversed__():
             newMinima = self.findMinima(row, lastMinima)
             lastMinima = newMinima
-            print dim.data[numpy.array(newMinima)]
+            print dim.data[numpy.array(newMinima)+1],len(newMinima)
             #origExtremaPos = newOrigExtremaPos
-        roots = DataContainer.FieldContainer(dim.data[numpy.array(newMinima)],
+        roots = DataContainer.FieldContainer(dim.data[numpy.array(newMinima)+1],
                                              unit = dim.unit,
                                              longname="%s of the local %s of %s" % (dim.longname,"minima",field.longname),
                                              shortname="%s_0" % dim.shortname)
@@ -105,33 +107,14 @@ class MRA(Worker.Worker):
 def main():
     import pylab
     mra = MRA()
-    x = numpy.linspace(-2, 2, 100)
+    N=200
+    x = numpy.linspace(-2, 2, N)
     y = x**4-3*x**2+x
-    #z = y + scipy.random.randn(100)*0.5
-    z = numpy.array([ 2.34780571,  2.09156238,  0.5332234 , -0.02834118, -0.32603188,
-                      -0.60151411, -1.11347491, -2.08409331, -2.70819461, -3.18515954,
-                      -3.05347274, -3.34044027, -2.13135733, -4.01050254, -3.41987657,
-                      -3.6012123 , -3.78173869, -3.47931019, -3.24799948, -3.11049149,
-                      -3.5452045 , -3.06428498, -3.21358035, -2.96396225, -3.62057493,
-                      -2.72119203, -2.66721085, -2.82274751, -3.77335446, -2.69488486,
-                      -2.01602774, -1.68001615, -1.52803751, -2.82155386, -1.87232676,
-                      -1.4543144 , -0.81421548, -0.67173508, -0.67432948, -1.62994539,
-                      -0.70960285, -0.59510169, -0.77448565, -0.61368053, -0.46649733,
-                      1.24964375, -0.13614141, -0.47720227,  0.56392326,  0.76611274,
-                      0.09380934,  0.06581185,  0.8145737 , -0.51146898, -0.41842572,
-                      0.5122507 , -0.28047205,  0.99774688,  0.29451745,  0.47395212,
-                      0.24947649, -0.52780659, -0.87432683, -0.45071175,  0.13419426,
-                      -0.99334888, -0.19878416, -0.11586503, -0.03590624, -1.02419055,
-                      -0.12816827, -0.59284413, -1.83761451, -1.61972556, -0.56452747,
-                      -0.97584479, -0.69704826, -1.34867496, -0.48626359,  0.19873513,
-                      -1.25647259, -0.06859142, -1.44884286, -1.13005531, -1.28660869,
-                      -0.97218543, -0.58118687,  0.28563569, -0.31884048,  0.74143098,
-                      0.35096262,  1.71650232,  0.77247154,  2.35576553,  2.61386751,
-                      2.75560072,  3.11663764,  5.81077989,  6.06092933,  6.08185724])
+    z = y + scipy.random.randn(N)*0.5
     data = DataContainer.FieldContainer(z, dimensions=[DataContainer.FieldContainer(x)])
     #c = mra.convolve(data, 10)
     #pylab.plot(x, c)
-    mra.paramScale.value = "2"
+    mra.paramScale.value = "1.0"
     mrr, roots = mra.mra(data)
     pylab.plot(x, mrr[-1])
     for c in mrr:
