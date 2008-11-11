0b9815542dc86a3d1b1903f081191fe4c9ba3b35
liehr <liehr@3321fe91-ea1f-0410-9b6b-81c2d13ccd49>
2008-11-11 11:17:30 +0000

    Included Konrad Hinsens class NumerDict into the PhysicalQuantities module.
    git-svn-id: https://pyphant.svn.sourceforge.net/svnroot/pyphant/trunk@574 3321fe91-ea1f-0410-9b6b-81c2d13ccd49

--- src/pyphant/pyphant/quantities/PhysicalQuantities.py
+++ src/pyphant/pyphant/quantities/PhysicalQuantities.py
@@ -26,7 +26,59 @@ guarantee for the correctness of all entries in the unit
 table, so use this at your own risk.
 """
 
-from pyphant.quantities.NumberDict import NumberDict
+class NumberDict(dict):
+
+    """
+    Dictionary storing numerical values
+
+    Constructor: NumberDict()
+
+    An instance of this class acts like an array of number with
+    generalized (non-integer) indices. A value of zero is assumed
+    for undefined entries. NumberDict instances support addition,
+    and subtraction with other NumberDict instances, and multiplication
+    and division by scalars.
+    """
+    
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            return 0
+
+    def __coerce__(self, other):
+        if type(other) == type({}):
+            other = NumberDict(other)
+        return self, other
+
+    def __add__(self, other):
+        sum_dict = NumberDict()
+        for key in self.keys():
+            sum_dict[key] = self[key]
+        for key in other.keys():
+            sum_dict[key] = sum_dict[key] + other[key]
+        return sum_dict
+
+    def __sub__(self, other):
+        sum_dict = NumberDict()
+        for key in self.keys():
+            sum_dict[key] = self[key]
+        for key in other.keys():
+            sum_dict[key] = sum_dict[key] - other[key]
+        return sum_dict
+
+    def __mul__(self, other):
+        new = NumberDict()
+        for key in self.keys():
+            new[key] = other*self[key]
+        return new
+    __rmul__ = __mul__
+
+    def __div__(self, other):
+        new = NumberDict()
+        for key in self.keys():
+            new[key] = self[key]/other
+        return new
 
 import numpy.oldnumeric 
 def int_sum(a, axis=0):
