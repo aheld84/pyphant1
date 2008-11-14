acf680b3014e470ec56536459aaebd62f77108ef
liehr <liehr@3321fe91-ea1f-0410-9b6b-81c2d13ccd49>
2008-11-14 15:07:38 +0000

    Removed ANTLR based Full-Metadata-Format parser.
    git-svn-id: https://pyphant.svn.sourceforge.net/svnroot/pyphant/trunk@576 3321fe91-ea1f-0410-9b6b-81c2d13ccd49

--- src/workers/fmfile/fmfile/update_tokens.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-java -cp /Users/zklaus/src/antlr-3.0.1/lib/antlr-2.7.7.jar:/Users/zklaus/src/antlr-3.0.1/lib/stringtemplate-3.1b1.jar:/Users/zklaus/src/antlr-3.0.1/lib/antlr-3.0.1.jar org.antlr.Tool FMFpython.g FMFpythonTree.g
-echo tokens = { >tokens.py && sed 's/\([^=]*\)=\(.*\)/    \2 : "\1",/g' FMFpython.tokens >>tokens.py && echo } >>tokens.py
-
