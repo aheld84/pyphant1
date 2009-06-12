bc7b8d7722dce33d8ae7c9cb6e7edf787c99fcc2
Alexander Held <alexander.held@physik.uni-freiburg.de>
2009-06-12 12:05:54 +0200

    Enh: Added preliminary GreyScaleErosion Worker
    Parameters are not interpreted correctly yet.

---
+++ src/workers/ImageProcessing/ImageProcessing/GreyScaleErosion.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2006-2007, Rectorate of the University of Freiburg
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of the Freiburg Materials Research Center,
+#   University of Freiburg nor the names of its contributors may be used to
+#   endorse or promote products derived from this software without specific
+#   prior written permission.
+#
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+u"""
+Preliminary gradient worker
+"""
+
+__id__ = "$Id$"
+__author__ = "$Author$"
+__version__ = "$Revision$"
+# $Source$
+
+from pyphant.core import Worker, Connectors,\
+                         Param, DataContainer
+import ImageProcessing
+import numpy, copy
+from scipy import ndimage
+
+class GreyScaleErosion(Worker.Worker):
+    API = 2
+    VERSION = 1
+    REVISION = "$Revision$"[11:-1]
+    name = "GreyScaleErosion"
+    _bgdark = "dark"
+    _bgbright = "bright"
+    _sockets = [("image", Connectors.TYPE_IMAGE)]
+    _params = [("iterations", "Iterations", 1, None),
+               ("background",
+                "Background(dark/bright)",
+                [_bgdark, _bgbright],
+                None),
+               ("nbhsize", "Neighborhood size in pixels", 5, None)]
+
+    def erode(self, data, background, nbhsize):
+        return ndimage.grey_erosion(data, size=nbhsize)
+
+    @Worker.plug(Connectors.TYPE_IMAGE)
+    def even_background(self, image, subscriber=0):
+        iterations = self.paramIterations.value
+        background = self.paramBackground.value
+        nbhsize = self.paramNbhsize.value
+        i = 0
+        olddata = image.data
+        while i < iterations:
+            newdata = self.erode(olddata, background, nbhsize)
+            olddata = newdata
+            i += 1
+        result = DataContainer.FieldContainer(
+            newdata,
+            copy.deepcopy(image.unit),
+            copy.deepcopy(image.error),
+            copy.deepcopy(image.mask),
+            copy.deepcopy(image.dimensions),
+            image.longname,
+            image.shortname,
+            copy.deepcopy(image.attributes),
+            False)
+        result.seal()
+        return result
