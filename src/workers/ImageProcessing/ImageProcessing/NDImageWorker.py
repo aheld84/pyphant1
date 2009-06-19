a0026f6bef9f22d161b5a81668b1357cb5b997ef
Alexander Held <alexander.held@physik.uni-freiburg.de>
2009-06-19 10:59:22 +0200

    Enh: Added some filters to NDImage worker
    Added watershed_ift (not operating yet).
    Added maximum_filter

--- src/workers/ImageProcessing/ImageProcessing/NDImageWorker.py
+++ src/workers/ImageProcessing/ImageProcessing/NDImageWorker.py
@@ -44,26 +44,61 @@ import ImageProcessing
 import numpy, copy
 from scipy import (ndimage, interpolate)
 
+def pile(func, imagedata):
+    assert imagedata.ndim in [2, 3]
+    if imagedata.ndim == 2:
+        pile = [imagedata]
+    else:
+        pile = imagedata
+    pile = [func(data) for data in pile]
+    if imagedata.ndim == 2:
+        newdata = pile[0]
+    else:
+        newdata = numpy.array(pile)
+    return newdata
+
+
 class NDImage(Worker.Worker):
     API = 2
     VERSION = 1
     REVISION = "$Revision$"[11:-1]
     name = "ndimage"
     _sockets = [("image", Connectors.TYPE_IMAGE)]
-    _params = [("2donly", "Use 2d filters only:", True, None),
-               ("ndfilter", "Filter (Binary Closing/Binary Opening)",
-                ["binary_closing",
-                 "binary_opening"], None),
-               ("iterations", "Iterations", 1, None),
-               ("kernel_size", "Kernel size", 5, None)]
+    _filters = {"binary_closing":("iterations", ),
+                "binary_opening":("iterations", ),
+                "binary_fill_holes":(),
+                #"watershed_ift":(None, ),
+                "maximum_filter":("size", )}
+    _ndparams = {"iterations":1,
+                 "size":5}
+    _params = [("pile", "Treat 3d images as pile of 2d images", True, None),
+               ("ndfilter", "Filter", _filters.keys(), None)]
+    _params += [(pn, pn, dflt, None) for pn, dflt in _ndparams.iteritems()]
 
-    def applyfilter(self, ndfilter):
-        pass
+    def watershed_ift(self, data):
+        return ndimage.watershed_ift(data, data)
+
+    def applyfilter(self, data):
+        fltparams = ''
+        for par in self._filters[self.paramNdfilter.value]:
+            if par != None:
+                fltparams += ', %s=%s' % (par, self.getParam(par).value)
+        if None in self._filters[self.paramNdfilter.value]:
+            call = "self"
+        else:
+            call = "ndimage"
+        todostr = "%s.%s(data%s)" % (call, self.paramNdfilter.value,
+                                     fltparams)
+        return eval(todostr)
 
     @Worker.plug(Connectors.TYPE_IMAGE)
     def ndimage(self, image, subscriber=0):
+        if self.paramPile.value:
+            newdata = pile(self.applyfilter, image.data)
+        else:
+            newdata = self.applyfilter(image.data)
         result = DataContainer.FieldContainer(
-            255 - image.data,
+            newdata,
             copy.deepcopy(image.unit),
             copy.deepcopy(image.error),
             copy.deepcopy(image.mask),
