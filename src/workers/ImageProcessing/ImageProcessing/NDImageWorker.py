3d1c81d74521e7bed042f14e86b9d7dbea7ebec5
Alexander Held <alexander.held@physik.uni-freiburg.de>
2009-06-19 17:36:59 +0200

    Enh: Added new filters to NDImage worker
    Added several new filters:
    median_filter
    cut_histogram - Replaces all unusally dark pixels by the mean value of the
                    image. This is done by examining the histogram.
    gradient
    label - returns an image with unique colors for each connected component.

--- src/workers/ImageProcessing/ImageProcessing/NDImageWorker.py
+++ src/workers/ImageProcessing/ImageProcessing/NDImageWorker.py
@@ -42,16 +42,20 @@ from pyphant.core import Worker, Connectors,\
                          Param, DataContainer
 import ImageProcessing
 import numpy, copy
-from scipy import (ndimage, interpolate)
+from scipy import (ndimage, interpolate, vectorize)
 
-def pile(func, imagedata):
+def pile(func, imagedata, runs=1, dopile=True):
     assert imagedata.ndim in [2, 3]
-    if imagedata.ndim == 2:
+    assert runs >= 0
+    if runs == 0:
+        return imagedata
+    if imagedata.ndim == 2 or not dopile:
         pile = [imagedata]
     else:
         pile = imagedata
-    pile = [func(data) for data in pile]
-    if imagedata.ndim == 2:
+    for run in xrange(runs):
+        pile = [func(data) for data in pile]
+    if imagedata.ndim == 2 or not dopile:
         newdata = pile[0]
     else:
         newdata = numpy.array(pile)
@@ -68,35 +72,75 @@ class NDImage(Worker.Worker):
                 "binary_opening":("iterations", ),
                 "binary_fill_holes":(),
                 #"watershed_ift":(None, ),
-                "maximum_filter":("size", )}
+                "maximum_filter":("size", "mode", "cval"),
+                "median_filter":("size", "mode", "cval"),
+                "grey_closing":("size", "mode", "cval"),
+                "grey_dilation":(None, "size", "mode", "cval"),
+                "grey_erosion":("size", "mode", "cval"),
+                "grey_opening":("size", "mode", "cval"),
+                "cut_histogram":(None, "tolerance"),
+                "gradient":(None, ),
+                "label":(None, "connectivity")}
     _ndparams = {"iterations":1,
-                 "size":5}
+                 "size":5,
+                 "mode":["reflect",
+                         "nearest",
+                         "wrap",
+                         "constant"],
+                 "cval":0,
+                 "tolerance":1000,
+                 "connectivity":2}
     _params = [("pile", "Treat 3d images as pile of 2d images", True, None),
                ("ndfilter", "Filter", _filters.keys(), None)]
     _params += [(pn, pn, dflt, None) for pn, dflt in _ndparams.iteritems()]
 
-    def watershed_ift(self, data):
-        return ndimage.watershed_ift(data, data)
+    #def watershed_ift(self, data):
+        #return ndimage.watershed_ift(data, data)
+
+    def gradient(self, data):
+        res = numpy.sqrt(sum(
+                numpy.square(numpy.array(numpy.gradient(data)))
+                ))
+        return (res * 255.0).astype(int) / 361
+
+    def grey_dilation(self, data, size, mode):
+        return 255 - ndimage.grey_erosion(255 - data, size=size, mode=mode)
+
+    def label(self, data, connectivity):
+        structure = ndimage.morphology.generate_binary_structure(data.ndim,
+                                                               connectivity)
+        return ndimage.label(data, structure=structure)[0]
+
+    def cut_histogram(self, data, tolerance):
+        h = ndimage.histogram(data, 0, 256, 256)
+        cs = numpy.cumsum(h)
+        cut = cs[255] / tolerance
+        for i in xrange(len(cs)):
+            if cs[i] > cut:
+                newmin = i
+                break
+        m = data.mean()
+        return numpy.where(data < newmin, m, data)
 
     def applyfilter(self, data):
-        fltparams = ''
+        if None in self._filters[self.paramNdfilter.value]:
+            call = getattr(self, self.paramNdfilter.value)
+        else:
+            call = getattr(ndimage, self.paramNdfilter.value)
+        args = {}
         for par in self._filters[self.paramNdfilter.value]:
             if par != None:
-                fltparams += ', %s=%s' % (par, self.getParam(par).value)
-        if None in self._filters[self.paramNdfilter.value]:
-            call = "self"
-        else:
-            call = "ndimage"
-        todostr = "%s.%s(data%s)" % (call, self.paramNdfilter.value,
-                                     fltparams)
-        return eval(todostr)
+                args[par] = self.getParam(par).value
+        print args
+        return call(data, **args)
 
     @Worker.plug(Connectors.TYPE_IMAGE)
     def ndimage(self, image, subscriber=0):
-        if self.paramPile.value:
-            newdata = pile(self.applyfilter, image.data)
+        if "iterations" in self._filters[self.paramNdfilter.value]:
+            runs = 1
         else:
-            newdata = self.applyfilter(image.data)
+            runs = self.paramIterations.value
+        newdata = pile(self.applyfilter, image.data, runs, self.paramPile.value)
         result = DataContainer.FieldContainer(
             newdata,
             copy.deepcopy(image.unit),
