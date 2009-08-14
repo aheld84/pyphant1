bd1f1c6525cf749e909a0412d0e635c909daa812
Alexander Held <alexander.held@physik.uni-freiburg.de>
2009-08-14 11:32:31 +0200

    Enh: More options for parameter mapping in BatchWorker
    The parameter destination is not restricted to the input worker anymore.
    Now there is an option to select any parameter of any worker in the recipe
    to remote control with the BatchWorker.
    There is also a new switch that allows to convert parameters to strings.
    (Useful for parameters with units as they usually are stored as user input
    strings)

--- src/workers/tools/tools/BatchWorker.py
+++ src/workers/tools/tools/BatchWorker.py
@@ -50,7 +50,8 @@ PARAMBY = [u"don't map",
            u"source/dest",
            u"columns of input SC"]
 
-def batch(inputSC, recipe, plug, paramMethod, paramIn, paramOut):
+def batch(inputSC, recipe, plug,
+          paramMethod, paramSource, paramDestWorker, paramDest, paramConvert):
     socket = recipe.getOpenSocketsForPlug(plug)[0]
     input_emd5s = inputSC['emd5'].data
     km = KM.getInstance()
@@ -58,7 +59,7 @@ def batch(inputSC, recipe, plug, paramMethod, paramIn, paramOut):
     emd5src = Emd5Src.Emd5Src()
     emd5src.paramSelectby.value = u"enter emd5"
     if paramMethod == PARAMBY[1]:
-            paramFC = inputSC[paramIn]
+            paramFC = inputSC[paramSource]
     rowcount = 0
     for input_emd5 in input_emd5s:
         input_dc = km.getDataContainer(input_emd5)
@@ -66,14 +67,16 @@ def batch(inputSC, recipe, plug, paramMethod, paramIn, paramOut):
             socket.pullPlug()
         emd5src.paramEnteremd5.value = unicode(input_emd5)
         if paramMethod == PARAMBY[1]:
-            fc = inputSC[paramIn]
+            fc = inputSC[paramSource]
             paramval = paramFC.data[rowcount]
             try:
                 withUnit = paramval * paramFC.unit
                 paramval = withUnit
             except:
                 pass
-            socket.worker.getParam(paramOut).value = paramval
+            if paramConvert:
+                paramval = paramval.__str__()
+            paramDestWorker.getParam(paramDest).value = paramval
         socket.insert(emd5src.getPlugs()[0])
         output_dc = plug.getResult()
         output_dc.seal()
@@ -100,16 +103,21 @@ class BatchWorker(Worker.Worker):
     name = "Batch"
     _params = [("worker", "Output worker (input via single open socket)",
                 [u"None"], None),
-               ("plug", "Output plug (press OK to update)", [u"None"], None),
+               ("plug", "Output plug (OK to update)", [u"None"], None),
                ("paramBy", "Map parameters via", PARAMBY, None),
-               ("paramIn", "Parameter source", [u"None"], None),
-               ("paramOut",
-                "Parameter destination (press OK to update)", [u"None"], None)]
+               ("paramSource", "Parameter source", [u"None"], None),
+               ("paramDestWorker", "Parameter destination worker",
+                [u"None"], None),
+               ("paramDest",
+                "Parameter destination (OK to update)", [u"None"], None),
+               ("paramConvert", "Convert parameter to string", False, None)]
     _sockets = [("emd5SC", Connectors.TYPE_ARRAY)]
 
     def refreshParams(self, subscriber = None):
         wlist = self.parent.getWorkers()
         self.paramWorker.possibleValues = [unicode(
+                worker.getParam('name').value) for worker in wlist]
+        self.paramParamDestWorker.possibleValues = [unicode(
                 worker.getParam('name').value) for worker in wlist]
         try:
             worker = self.parent.getWorker(
@@ -120,15 +128,15 @@ class BatchWorker(Worker.Worker):
         self.paramPlug.possibleValues = [unicode(plug.name) for plug in plist]
         if self.socketEmd5SC.isFull():
             tempSC = self.socketEmd5SC.getResult(subscriber)
-            self.paramParamIn.possibleValues = [unicode(
+            self.paramParamSource.possibleValues = [unicode(
                     key) for key in tempSC.longnames.keys()]
         try:
-            parout = self.parent.getOpenSocketsForPlug(
-                plug)[0].worker.getParamList()
-            outlist = [unicode(param.name) for param in parout]
+            pardest = self.parent.getWorker(
+                self.paramParamDestWorker.value.encode('utf-8')).getParamList()
+            destlist = [unicode(param.name) for param in pardest]
         except:
-            outlist = [u"None"]
-        self.paramParamOut.possibleValues = outlist
+            destlist = [u"None"]
+        self.paramParamDest.possibleValues = destlist
 
     @Worker.plug(Connectors.TYPE_ARRAY)
     def execute(self, emd5SC, subscriber = 0):
@@ -136,6 +144,18 @@ class BatchWorker(Worker.Worker):
         worker = recipe.getWorker(self.paramWorker.value.encode('utf-8'))
         plug = worker._plugs[self.paramPlug.value.encode('utf-8')]
         paramMethod = self.paramParamBy.value
-        paramIn = unicode(self.paramParamIn.value).encode('utf-8')
-        paramOut = unicode(self.paramParamOut.value).encode('utf-8')
-        return batch(emd5SC, recipe, plug, paramMethod, paramIn, paramOut)
+        if paramMethod != PARAMBY[0]:
+            paramDestWorker = recipe.getWorker(
+                self.paramParamDestWorker.value.encode('utf-8'))
+        else:
+            paramDestWorker = None
+        if paramMethod == PARAMBY[1]:
+            paramSource = unicode(self.paramParamSource.value).encode('utf-8')
+            paramDest = unicode(self.paramParamDest.value).encode('utf-8')
+        else:
+            paramSource = None
+            paramDest = None
+        paramConvert = self.paramParamConvert.value
+        return batch(emd5SC, recipe, plug,
+                     paramMethod, paramSource,
+                     paramDestWorker, paramDest, paramConvert)
