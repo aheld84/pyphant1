a086529c00e015cf40b55e2f5a4f79c25467b95f
Alexander Held <alexander.held@fmf.uni-freiburg.de>
2010-05-25 15:47:57 +0200

    Enh: Added recipe path to title bar
    Added recipe path to title bar in wxPyphant
    Also adapted "save as" to update the path in the
    titlebar

--- src/pyphant/pyphant/wxgui2/wxPyphantApplication.py
+++ src/pyphant/pyphant/wxgui2/wxPyphantApplication.py
@@ -117,7 +117,8 @@ class wxPyphantFrame(wx.Frame):
     ID_VIEW_LOGFILE = wx.NewId()
 
     def __init__(self, _wxPyphantApp):
-        wx.Frame.__init__(self, None, -1, "wxPyphant %s" % __version__,
+        self.titleStr = "wxPyphant %s | Recipe: %s" % (__version__, "%s")
+        wx.Frame.__init__(self, None, -1, self.titleStr % "None",
                           size=(640,480))
         import PyphantCanvas
         self._statusBar = self.CreateStatusBar()
@@ -237,6 +238,9 @@ class wxPyphantFrame(wx.Frame):
                     self._remainingSpace = PyphantCanvas.PyphantCanvas(self, recipe)
                 else:
                     self._remainingSpace = PyphantCanvas.PyphantCanvas(self)
+                from pyphant.core.WebInterface import shorten
+                self.SetTitle(self.titleStr \
+                              % shorten(self._wxPyphantApp.pathToRecipe, 30, 30))
             else:
                 raise IOError('Unknown file format in file "%s"'\
                               % self._wxPyphantApp.pathToRecipe)
@@ -275,6 +279,8 @@ class wxPyphantFrame(wx.Frame):
                 self._fileMenu.IsChecked(wx.ID_FILE4))
             self._wxPyphantApp.pathToRecipe = filename
             self.recipeState = 'clean'
+            from pyphant.core.WebInterface import shorten
+            self.SetTitle(self.titleStr % shorten(filename, 30, 30))
         else:
             dlg.Destroy()
 
